60552297bac532eb89ff046f3644f8ff9051c53e
Andy Grove <andygrove73@gmail.com>
2020-07-19 10:39:01 -0600

    skeleton for aggregate

---
+++ rust/ballista/src/execution/expressions/count.rs
@@ -0,0 +1,61 @@
+// Copyright 2020 Andy Grove
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::Arc;
+
+use crate::arrow::datatypes::{DataType, Schema};
+use crate::error::Result;
+use crate::execution::physical_plan::{
+    Accumulator, AggregateExpr, AggregateMode, ColumnarBatch, ColumnarValue, Expression,
+};
+
+#[derive(Debug)]
+pub struct Count {
+    input: Arc<dyn Expression>,
+}
+
+impl Count {
+    pub fn new(input: Arc<dyn Expression>) -> Self {
+        Self { input }
+    }
+}
+
+impl AggregateExpr for Count {
+    fn name(&self) -> String {
+        unimplemented!()
+    }
+
+    fn data_type(&self, _input_schema: &Schema) -> Result<DataType> {
+        unimplemented!()
+    }
+
+    fn nullable(&self, _input_schema: &Schema) -> Result<bool> {
+        unimplemented!()
+    }
+
+    fn evaluate_input(&self, _batch: &ColumnarBatch) -> Result<ColumnarValue> {
+        unimplemented!()
+    }
+
+    fn create_accumulator(&self, _mode: &AggregateMode) -> Rc<RefCell<dyn Accumulator>> {
+        unimplemented!()
+    }
+}
+
+/// Create a count expression
+pub fn count(expr: Arc<dyn Expression>) -> Arc<dyn AggregateExpr> {
+    Arc::new(Count::new(expr))
+}
